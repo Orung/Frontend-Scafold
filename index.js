34f0f2f66cb9aa5a17466387bbeb075838321b25
c_orungpro <promise.orung@ng.ab-inbev.com>
2023-09-18 15:34:03 +0100

    Implement scarfold

--- index.js
+++ index.js
@@ -17,8 +17,20 @@ if (!fs.existsSync(projectDir)) {
 if (!fs.existsSync(htmlDir)) {
     fs.mkdirSync(htmlDir);
     console.log(`Created HTML directory: ${htmlDir}`);
-    const htmlContent = `<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="./css/style.css"> <title>Document</title> </head> <body> <h1>Welcome</h1> <script src="./js/script.js"></script> </body> </html>`;
-   
+    const htmlContent = `<!DOCTYPE html> 
+    <html lang="en"> 
+    <head> 
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css"> 
+    <title>Document</title> 
+    </head> 
+    <body> 
+    <h1>Welcome</h1> 
+    <script src="./js/script.js"></script> 
+    </body>
+    </html>`;
+
     fs.writeFileSync(path.join(htmlDir, 'index.html'), htmlContent);
     console.log(`Created HTML file: ${path.join(htmlDir, 'index.html')}`);
 } else {
@@ -30,23 +42,23 @@ if (!fs.existsSync(htmlDir)) {
 if (!fs.existsSync(cssDir)) {
     fs.mkdirSync(cssDir);
     console.log(`Created CSS directory: ${cssDir}`);
-  
+    
     const cssContent = 'h1 {text-align:center; }';
 
     fs.writeFileSync(path.join(cssDir, 'style.css'), cssContent);
     console.log(`Created CSS file: ${path.join(cssDir, 'style.css')}`);
-  } else {
+} else {
     console.error(`CSS directory "${cssDir}" already exists.`);
-  }
- 
+}
 
-  if (!fs.existsSync(jsDir)) {
+
+if (!fs.existsSync(jsDir)) {
     fs.mkdirSync(jsDir);
     console.log(`Created JS directory: ${jsDir}`);
-  
+
     const jsContent = 'alert("Welcome");';
     fs.writeFileSync(path.join(jsDir, 'script.js'), jsContent);
     console.log(`Created JS file: ${path.join(jsDir, 'script.js')}`);
-  } else {
+} else {
     console.error(`JS directory "${jsDir}" already exists.`);
-  }
+}
